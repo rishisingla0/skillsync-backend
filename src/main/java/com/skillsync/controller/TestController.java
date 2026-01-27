0e9afccc6e7e9b6e29abd95413d36866ba0f0d33
Rishi Singla <rishisingla36@gmail.com>
2026-01-27 21:55:14 +0530

    Day 1:Spring boot project setup and health check api

---
+++ src/main/java/com/skillsync/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.skillsync.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/health")
+    public String healthcheck() {
+        return "Skillsync backend is running!";
+    }
+}
